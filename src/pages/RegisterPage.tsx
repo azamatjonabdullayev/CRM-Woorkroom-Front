3245325c98a5c70dc43d4fe2bf964cfecc13e46a
Azamat <abdullayevazamat2705@gmail.com>
2025-08-05 14:08:05 +0500

    added vercel.json

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { Camera } from "lucide-react";
 
 const RegisterPage: React.FC = () => {
   return (
     <div className="h-screen bg-[#f3f9fd] py-5 pl-5 pr-10 flex items-center gap-7">
       <section className="left-side w-[30%] h-full bg-[#3f8cff] rounded-3xl"></section>
 
-      <section className="right-side w-full h-full bg-white rounded-3xl"></section>
+      <section className="right-side w-full h-full bg-white rounded-3xl">
+        <Camera color="red" />
+      </section>
     </div>
   );
 };
